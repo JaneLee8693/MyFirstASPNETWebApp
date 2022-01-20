9c722e06ab8c2bfec32a9526347429482485664e
Jane Lee <xo930497@yahoo.com.tw>
2022-01-19 20:29:27 -0700

    mission3

---
+++ MyFirstASPNETWebApp/Models/GradeCalculatorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNETWebApp.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required(ErrorMessage = "Pleasee enter valid number")]
+        [Range(0, 100, ErrorMessage = "The length must be between 0 to 100")]
+        public float Assignments { get; set; }
+
+        
+        [Range(0, 100, ErrorMessage = "The length must be between 0 to 100")]
+        public float Project { get; set; }
+
+        
+        [Range(0, 100, ErrorMessage = "The length must be between 0 to 100")]
+        public float Quizzes { get; set; }
+
+        
+        [Range(0, 100, ErrorMessage = "The length must be between 0 to 100")]
+        public float Exams { get; set; }
+
+        
+        [Range(0, 100, ErrorMessage = "The length must be between 0 to 100")]
+        public float Intex { get; set; }
+    }
+}
